5b8bac8a9840d2b878a93faad3d47e16aef169fe
moussadem7140 <moussadembl@hotmail.com>
2025-02-16 22:46:17 -0500

    Ajout des classes néccesaires pour le sprint_1

---
+++ Projet_Gestion_Finance/Models/Dal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Gestion_Finance.Models
+{
+    public static class Dal
+    {
+
+    }
+}
