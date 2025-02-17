b705802981c62e850d6c2f1c169fae488280a92d
moussadem7140 <moussadembl@hotmail.com>
2025-02-16 22:51:40 -0500

    ClasseCategorie

--- Projet_Gestion_Finance/Models/Categorie.cs
+++ Projet_Gestion_Finance/Models/Categorie.cs
@@ -8,5 +8,33 @@ namespace Projet_Gestion_Finance.Classes
 {
     public class Categorie
     {
+		private string _nom;
+
+		public string Nom
+		{
+			get { return _nom; }
+			set { _nom = value; }
+		}
+		private decimal _limiteDepenses;
+
+		public decimal LimiteDepenses
+		{
+			get { return _limiteDepenses; }
+			set { _limiteDepenses = value; }
+		}
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+        public Categorie(string nom, decimal limiteDepenses)
+        {
+            Nom = nom;
+            LimiteDepenses = limiteDepenses;
+            Description = $"{nom}:  ";	
+        }
     }
 }
