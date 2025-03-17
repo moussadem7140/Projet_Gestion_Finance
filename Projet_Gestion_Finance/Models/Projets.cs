5fd8a3d8e823494fc68ed4de46d16849f4ae38ba
moussadem7140 <moussadembl@hotmail.com>
2025-03-17 10:56:51 -0400

    [Projets]Fonction qui retourne l'avancement

--- Projet_Gestion_Finance/Models/Projets.cs
+++ Projet_Gestion_Finance/Models/Projets.cs
@@ -9,49 +9,89 @@ namespace Projet_Gestion_Finance.Models
 {
     public class Projets
     {
-		private int _id;
+        private int _id;
 
-		public int Id
+        public int Id
         {
             get { return _id; }
             set { _id = value; }
         }
-		private string _nom;
+        private string _nom;
 
-		public string Nom
-		{
-			get { return _nom; }
-			set { _nom = value; }
-		}
-		private decimal _cout;
-
-		public decimal Cout
-		{
-			get { return _cout; }
-			set { _cout = value; }
-		}
-		private Depenses.TypeFrequence _frequence;
-
-		public int Frequence
+        public string Nom
         {
-            get { return (int)_frequence; }
-            set { _frequence = (Depenses.TypeFrequence)value; }
+            get { return _nom; }
+            set { _nom = value; }
         }
-		private DateTime _date;
+        private decimal _cout;
 
-		public DateTime Date
-		{
-			get { return _date; }
-			set { _date = value; }
-		}
-
-        public Projets(int id, string nom, DateTime date, decimal cout, int frequence)
+        public decimal Cout
         {
-			Id = id;
+            get { return _cout; }
+            set { _cout = value; }
+        }
+        private Depenses.TypeFrequence _frequence;
+
+        public Depenses.TypeFrequence Frequence
+        {
+            get { return _frequence; }
+            set { _frequence = value; }
+        }
+        private DateTime _date;
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+        private decimal _objectif;
+
+        public decimal Objectif
+        {
+            get { return _objectif; }
+            set { _objectif = value; }
+        }
+
+        public Projets(int id, string nom, DateTime date, decimal objectif, decimal cout, Depenses.TypeFrequence frequence)
+        {
+            Id = id;
             Nom = nom;
             Cout = cout;
+            Objectif = objectif;
             Frequence = frequence;
             Date = date;
+        }
+        public decimal Avancement(DateTime date)
+        {
+            decimal avancement = 0;
+            if (Frequence == Depenses.TypeFrequence.Mensuel)
+            {
+                int i = 0;
+                while (date.AddMonths(i) <= Date)
+                {
+                    avancement += Cout;
+                    i++;
+                }
+            }
+            if (Frequence == Depenses.TypeFrequence.Hebdomadaire)
+            {
+                int i = 0;
+                while (date.AddDays(i * 7) <= Date)
+                {
+                    avancement += Cout;
+                    i++;
+                }
+            }
+            else
+            {
+                int i = 0;
+                while (date.AddYears(i) <= Date)
+                {
+                    avancement += Cout;
+                    i++;
+                }
+            }
+            return avancement;
         }
     }
 }
